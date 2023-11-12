de786f251366614de93a01da7d7d9332c09b249a
Katie Kirby <jackalhandz@protonmail.com>
2023-11-11 22:04:20 -0800

    Update app.py

--- huggingface2/app.py
+++ huggingface2/app.py
@@ -60,9 +60,10 @@ def text2speech(message):
         print("Audio file saved as 'audio.mp3'")
     else:
         print(f"Error: {response.status_code} - {response.text}")
-
+        
+Uimage= "https://freeimage.host/i/JCqUD1s"
 # Call the function with your message
-scenario = img2text("https://freeimage.host/i/JCqUD1s")
+scenario = img2text("Uimage")
 story = generate_story(scenario)
 text2speech(story)
 
